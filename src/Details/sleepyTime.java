809d87f4b195be0351c0c18846c28d1ade388d34
calebology <calebmahweekeat@gmail.com>
2021-02-09 13:33:10 +0800

    Added better formatting

--- src/Details/sleepyTime.java
+++ src/Details/sleepyTime.java
@@ -1,10 +1,12 @@
 package Details;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 import org.joda.time.DateTime;
 import org.joda.time.Duration;
+import org.joda.time.Hours;
 import org.joda.time.format.DateTimeFormat;
 
 public class sleepyTime {
@@ -21,7 +23,6 @@ public class sleepyTime {
 		//Specifying parameters for average time it takes to fall asleep (15m)
 		fall_asleep = fall_asleep.minuteOfHour().setCopy(15);
 		
-		
 		wake_time();
 	}
 	public static void wake_time() {
@@ -33,19 +34,25 @@ public class sleepyTime {
 		org.joda.time.format.DateTimeFormatter dt = DateTimeFormat.forPattern("HH:mm");
 				
 		DateTime joda_wake = dt.parseDateTime(wake);
-		
+				
 		
 		for (int i = 0; i <= 6; i++) {
 			joda_wake = joda_wake.plusHours(1).plusMinutes(30);
-			System.out.println(joda_wake);
+			if ((boolean)(Math.log10(joda_wake.getMinuteOfHour()+1) == 1)){
+				System.out.println("In order to obtain " + i + " sleep cycle's worth of sleep, set your alarm at 0" + joda_wake.getHourOfDay() + ":0" + joda_wake.getMinuteOfHour());
+			} 
+			
+			else {
+				System.out.println("In order to obtain " + i + " sleep cycle's worth of sleep, set your alarm at 0" + joda_wake.getHourOfDay() + ":" + joda_wake.getMinuteOfHour() );
+			}
+
 		}
 		
-		System.out.println(joda_wake.clock;
-		System.out.println(joda_wake.getMinuteOfDay());
+}
+		
 		
 	
 	
-	}
 		
 		
 
